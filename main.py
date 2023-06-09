56d9bf61310751fe19b5347e296a78995b7257d2
SickHr <blawatjustin@gmail.com>
2023-06-09 21:44:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ from sys import argv
 
 link = argv[1]
 yt = YouTube(link)
+yd = yt.streams.get_highest_resolution()
+
+
 
 print("Title: ", yt.title)
+print("Number of views: ", yt.views)
+print("Length of video: ", yt.length, "seconds")
+
+
+download_path = "C:/Users/blawa/Desktop/youtube_download"
+
+yd.download(download_path)
+
